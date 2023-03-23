82e6767aba4056feca7b7043b5464d31edac3e0a
Андрей Щипцов <Bigbrotherx@yandex.ru>
2023-03-23 12:23:54 +0200

    docstrings added, deleted unused permitions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,18 +1,6 @@
 from rest_framework import permissions
 
 
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.user.is_superuser:
-            return True
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated:
-            return obj.author == request.user
-
-
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_superuser:
@@ -29,20 +17,6 @@ class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return request.user.role == "admin" or request.user.is_superuser
-
-
-class IsAdminOrModeratorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.user.is_superuser:
-            return True
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return bool(
-                request.user.role == "admin"
-                or request.user.role == "moderator"
-                or request.user.is_superuser
-            )
 
 
 class IsAdOrModOrAuthorOrReadOnly(permissions.BasePermission):
