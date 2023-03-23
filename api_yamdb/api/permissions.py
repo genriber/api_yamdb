b06736dfb7c87ee2752f25c90207791ec6c5fe62
Алексей Каргаев <genriber@gmail.com>
2023-03-23 17:47:37 +0400

    permissions.py improooooove

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -8,20 +8,23 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        if request.user.is_superuser:
-            return True
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return request.user.role == "admin"
+        return (
+            request.user.is_superuser
+            or request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+            and request.user.role == "admin"
+        )
 
 
 class AdminOnly(permissions.BasePermission):
     """Премишен админа или суперпользователя"""
 
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return request.user.role == "admin" or request.user.is_superuser
+        return (
+            request.user.is_authenticated
+            and request.user.role == "admin"
+            or request.user.is_superuser
+        )
 
 
 class IsAdOrModOrAuthorOrReadOnly(permissions.BasePermission):
@@ -31,21 +34,19 @@ class IsAdOrModOrAuthorOrReadOnly(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        if request.user.is_superuser:
-            return True
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return True
+        return (
+            request.user.is_superuser
+            or request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser:
-            return True
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return bool(
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+            and (
                 request.user.role == "moderator"
                 or request.user.role == "admin"
                 or obj.author == request.user
             )
+        )
